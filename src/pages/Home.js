76c97a160d1ec7a2af7fca711ab47196aa32b862
SooBin An <jejupalette@naver.com>
2022-09-04 00:06:08 +0900

    fix: mock 상품 데이터 불러와 홈 화면에 노출되도록 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import ProductBrief from '../components/ProductBrief'
 import styled from 'styled-components'
 import Header from '../components/Header'
 import ThemeButton from '../components/ThemeButton'
+import { mockTheme1Produdcts, mockTheme2Produdcts } from '../data/mockData'
 
 const ThinLine = styled.hr`
   border: solid 1px rgba(238, 238, 238, 1);
@@ -22,6 +23,7 @@ const ProductSection = styled.div`
   padding-bottom: 82px;
 `
 const Home = () => {
+  console.log(mockTheme1Produdcts)
   return (
     <div>
       <Header />
@@ -32,33 +34,13 @@ const Home = () => {
       </ThemeSection>
       <BoldLine />
       <ProductSection>
-        <ProductBrief
-          img={
-            'https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg'
-          }
-          productName={'비숑 블랙 머그잔'}
-          productIntroduction={
-            '쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.'
-          }
-        />
-        <ProductBrief
-          img={
-            'https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg'
-          }
-          productName={'가열 보온 티코스터 온열 원터치 컵 받침대'}
-          productIntroduction={
-            '언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따뜻한 차를 즐길 수 있습니다.'
-          }
-        />
-        <ProductBrief
-          img={
-            'https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg'
-          }
-          productName={'벨루즈까사 솜사탕 파스텔 머그 4종 세트'}
-          productIntroduction={
-            '솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그'
-          }
-        />
+        {mockTheme1Produdcts.map((product) => (
+          <ProductBrief
+            img={product.thumbnail}
+            productName={product.name}
+            productIntroduction={product.description}
+          />
+        ))}
       </ProductSection>
     </div>
   )
