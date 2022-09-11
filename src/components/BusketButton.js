1467a98f3fa0b3a17708f1129b805c7ce0eb049f
SooBin An <jejupalette@naver.com>
2022-09-12 00:31:11 +0900

    feat : 상품세부페이지와 장바구니에서 장바구니 버튼 형태 다르게 수정

--- src/components/BusketButton.js
+++ src/components/BusketButton.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { useLocation } from 'react-router-dom'
 
 const Button = styled.button`
   width: 390px;
@@ -7,12 +8,16 @@ const Button = styled.button`
   font-size: 16px;
   font-weight: 700;
   border: none;
+  cursor: pointer;
 `
 
 const BusketButton = ({ onClick }) => {
+  const location = useLocation()
   return (
     <div>
-      <Button onClick={onClick}>장바구니 담기</Button>
+      <Button onClick={onClick}>
+        {location.pathname === '/basket' ? '주문하기' : '장바구니 담기'}
+      </Button>
     </div>
   )
 }
