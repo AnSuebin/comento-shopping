659ba734f96bc418829b9272bbcf9ce020eb064a
SooBin An <jejupalette@naver.com>
2022-09-11 20:41:57 +0900

    design : BasketItem 배치 수정

--- src/components/BasketItem.js
+++ src/components/BasketItem.js
@@ -5,22 +5,27 @@ const BasketItemStyled = styled.div`
   height: 126px;
   padding: 15px;
   display: grid;
+  grid-gap: 16px;
   grid-template-columns: 1fr 2fr;
+  grid-template-areas: 'ProductImage  LayoutText';
 `
 
 const ProductImage = styled.img`
   object-fit: cover;
   width: 120px;
   height: 100px;
+  grid-area: ProductImage;
 `
 
 const LayoutText = styled.div`
+  display: grid;
   grid-template-columns: 2fr 1fr;
   grid-template-rows: 1fr 1fr;
   grid-row: 16px;
   grid-template-areas:
     'ProductTitle CloseButton'
-    'ProductPrice ProductPrice';
+    'ProductPrice .';
+  grid-area: LayoutText;
 `
 
 const ProductTitle = styled.h2`
@@ -29,6 +34,7 @@ const ProductTitle = styled.h2`
   line-height: 21px;
   letter-spacing: -0.01em;
   color: #000000;
+  margin: 0;
   grid-area: ProductTitle;
 `
 
@@ -36,10 +42,13 @@ const CloseButton = styled.h3`
   font-weight: 700;
   font-size: 20px;
   grid-area: CloseButton;
+  margin: 0;
   display: flex;
   justify-content: flex-end;
+  align-items: flex-start;
 `
 const ProductPrice = styled.h3`
+  margin: 0;
   font-weight: 400;
   font-size: 16px;
   grid-area: ProductPrice;
@@ -51,14 +60,23 @@ const ThinLine = styled.hr`
   border: solid 1px rgba(238, 238, 238, 1);
 `
 
-const BasketItem = ({ Thumnail, productName, price, onClickRemoveButton }) => {
+const BasketItem = ({
+  Thumnail,
+  productName,
+  price,
+  // onClickRemoveButton
+}) => {
   return (
     <>
       <BasketItemStyled>
         <ProductImage src={Thumnail} alt={productName} />
         <LayoutText>
           <ProductTitle>{productName}</ProductTitle>
-          <CloseButton onClick={onClickRemoveButton}>x</CloseButton>
+          <CloseButton
+          //   onClick={onClickRemoveButton}
+          >
+            X
+          </CloseButton>
           <ProductPrice>{price}원</ProductPrice>
         </LayoutText>
         <ThinLine />
@@ -66,3 +84,5 @@ const BasketItem = ({ Thumnail, productName, price, onClickRemoveButton }) => {
     </>
   )
 }
+
+export default BasketItem
--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,5 +1,18 @@
+import styled from 'styled-components'
+import BasketItem from '../components/BasketItem'
+import ProductImage from '../utils/productBichonImgs/ProductDetailImg.png'
+
 const Basket = () => {
-  return <div></div>
+  return (
+    <div>
+      <BasketItem
+        Thumnail={ProductImage}
+        productName="비숑"
+        price="12000원"
+        // onClickRemoveButton={onClickRemoveButton}
+      />
+    </div>
+  )
 }
 
 export default Basket
