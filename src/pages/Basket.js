861a00f346ed19deb9a84435bf5b6675e98d1505
SooBin An <jejupalette@naver.com>
2022-09-12 00:33:47 +0900

    feat : 장바구니에 상품 관련 총합 목록 내용 수정 및 버튼 클릭 시 confirm모달 및 메인으로 돌아가도록 기능 추가

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -4,14 +4,51 @@ import BasketItem from '../components/BasketItem'
 import * as storage from '../utils/storage'
 import Header from '../components/Header'
 import styled from 'styled-components'
+import BusketButton from '../components/BusketButton'
+import { useNavigate } from 'react-router-dom'
 
 const BasketContainer = styled.div`
   width: 390px;
+`
+const BasketMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+  justify-content: space-between;
+`
+
+const BasketList = styled.div``
+
+const BuyingInfoUl = styled.div`
+  margin-bottom: 10px;
+`
+
+const BuyingInfoLi = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`
+const BuyingInfoTitle = styled.h5`
+  font-weight: 400;
+  font-size: 16px;
+  color: #616161;
+  margin: 0;
+`
+const BuyingInfo = styled.h5`
+  font-weight: 400;
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.86);
+  margin: 0;
 `
 
 const Basket = () => {
   const [basketItems, setBasketItems] = useState([])
   const [basketItemCount, setbasketItemCount] = useState(0)
+  // const [priceList, setPriceList] = useState([])
+  const [priceSum, setPriceSum] = useState(0)
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     //1. 장바구니 데이터 가져오기
@@ -25,6 +62,17 @@ const Basket = () => {
   useEffect(() => {
     const items = storage.getBasketItems()
     setBasketItems(items)
+    // 여기 왜 와야하는지 잘 모르겠음!
+    // 왜 여기 오지 않으면 바뀌지 않는지 잘 모르겠음!
+    setPriceSum(
+      items
+        .map(
+          // 1.원과 콤마 제거
+          // 2. 숫자로 변경
+          (item) => +item.price.slice(0, -1).replace(',', '')
+        )
+        .reduce((a, b) => a + b)
+    )
   }, [basketItemCount])
 
   const onClickRemoveButton = (productId) => {
@@ -33,22 +81,44 @@ const Basket = () => {
     // 상품 갯수 추가하면서 리랜더링하게 해주기
     setbasketItemCount(basketItemCount - 1)
   }
+  const onClickBusketButton = () => {
+    window.confirm('주문되었습니다.')
+    navigate('/')
+  }
 
   return (
     <BasketContainer>
       <Header />
-      {/* 2. UI 구현하기 */}
-      {basketItems &&
-        basketItems.map((product) => (
-          <BasketItem
-            key={product.id}
-            Thumnail={product.thumbnail}
-            productName={product.name}
-            price={product.price}
-            onClickRemoveButton={() => onClickRemoveButton(product.id)}
-          />
-        ))}
-      <div>{basketItemCount}</div>
+      <BasketMain>
+        <BasketList>
+          {/* 2. UI 구현하기 */}
+          {basketItems &&
+            basketItems.map((product) => (
+              <BasketItem
+                key={product.id}
+                Thumnail={product.thumbnail}
+                productName={product.name}
+                price={product.price}
+                onClickRemoveButton={() => onClickRemoveButton(product.id)}
+              />
+            ))}
+        </BasketList>
+        <BuyingInfoUl>
+          <BuyingInfoLi>
+            <BuyingInfoTitle>상품 금액 ({basketItemCount}개)</BuyingInfoTitle>
+            <BuyingInfo>{priceSum}원</BuyingInfo>
+          </BuyingInfoLi>
+          <BuyingInfoLi>
+            <BuyingInfoTitle>배송비</BuyingInfoTitle>
+            <BuyingInfo>{priceSum >= 30000 ? '0원' : '3500원'}</BuyingInfo>
+          </BuyingInfoLi>
+          <BuyingInfoLi>
+            <BuyingInfoTitle>총 주문 금액</BuyingInfoTitle>
+            <BuyingInfo>{priceSum}원</BuyingInfo>
+          </BuyingInfoLi>
+        </BuyingInfoUl>
+      </BasketMain>
+      <BusketButton onClick={onClickBusketButton} />
     </BasketContainer>
   )
 }
